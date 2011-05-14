a1a8e1c563c85ea298d12ea767d4454e70843222
Mikolaj Dobski <ankhazam@gmail.com>
2011-05-14 23:07:46 +0200

    Production factory will only include CUDA assisted computing on appropriate hardware.

--- core/src/main/java/org/put/hd/dmarf/algorithms/factories/ProductionAlgorithmFactory.java
+++ core/src/main/java/org/put/hd/dmarf/algorithms/factories/ProductionAlgorithmFactory.java
@@ -1,38 +1,48 @@
 package org.put.hd.dmarf.algorithms.factories;
 
 import java.util.LinkedList;
-import java.util.List;
 
+import java.util.List;
+import jcuda.*;
+import jcuda.driver.JCudaDriver;
+import jcuda.runtime.*;
 import org.put.hd.dmarf.algorithms.IAlgorithm;
 import org.put.hd.dmarf.algorithms.WekaAlgorithm;
 import org.put.hd.dmarf.algorithms.apriori.AprioriCudaAlgorithm;
 
 /**
- * Production factory for code.
- * NOTE: each algorithm in this factory will be tested at integration level by 
- * maven.
+ * Production factory for code. NOTE: each algorithm in this factory will be
+ * tested at integration level by maven.
+ * 
  * @author Piotr
- *
+ * 
  */
 public class ProductionAlgorithmFactory implements IAlgorithmFactory {
 
-	private List<IAlgorithm> algorithms;
-	
-	public ProductionAlgorithmFactory(){
-		algorithms = new LinkedList<IAlgorithm>();
-		
-		// Weka algorithm is the first one to be implemented in production phase.
-		algorithms.add(new WekaAlgorithm());
-		
-		
-	}
-	
-	public int getNumberOfAlgorithms() {
-		return algorithms.size(); 
+    private List<IAlgorithm> algorithms;
+
+    public ProductionAlgorithmFactory() {
+	algorithms = new LinkedList<IAlgorithm>();
+
+	// Weka algorithm is the first one to be implemented in production
+	// phase.
+	algorithms.add(new WekaAlgorithm());
+
+	// verifying that hardware is CUDA enabled
+	int count[] = new int[1];
+	if (JCuda.cudaGetDeviceCount(count) == cudaError.cudaSuccess
+		&& count[0] > 0) {
+	    algorithms.add(new AprioriCudaAlgorithm());
 	}
 
-	public IAlgorithm getAlgorithm(int number) {
-		return algorithms.get(number);
-	}
+    }
+
+    public int getNumberOfAlgorithms() {
+	return algorithms.size();
+    }
+
+    public IAlgorithm getAlgorithm(int number) {
+	return algorithms.get(number);
+    }
 
 }
