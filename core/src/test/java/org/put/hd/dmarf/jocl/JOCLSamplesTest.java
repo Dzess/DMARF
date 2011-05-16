f832a510e50060649743764d8663d4619019fc5f
Piotr Jessa <dzesse@gmail.com>
2011-05-16 21:33:10 +0200

    Truning off OpenCL tests, fixing issue with open file handler

--- core/src/test/java/org/put/hd/dmarf/jocl/JOCLSamplesTest.java
+++ core/src/test/java/org/put/hd/dmarf/jocl/JOCLSamplesTest.java
@@ -4,19 +4,20 @@ import org.junit.Test;
 import org.jocl.samples.*;
 
 public class JOCLSamplesTest {
+/*
+	@Test
+	public void deviceQueryTest() {
+		JOCLDeviceQuery.runJOCLDeviceQuery();
+	}
 
-    @Test
-    public void deviceQueryTest() {
-	JOCLDeviceQuery.runJOCLDeviceQuery();
-    }
+	@Test
+	public void OpenCL_1_0_Test() {
+		JOCLSample.runJOCL_1_0_Sample();
+	}
 
-    @Test
-    public void OpenCL_1_0_Test() {
-	JOCLSample.runJOCL_1_0_Sample();
-    }
-    
-    @Test
-    public void OpenCL_1_1_CPU_Test() {
-	JOCLSample_1_1.runJOCL_1_1_Sample();
-    }    
+	@Test
+	public void OpenCL_1_1_CPU_Test() {
+		JOCLSample_1_1.runJOCL_1_1_Sample();
+	}
+*/
 }
