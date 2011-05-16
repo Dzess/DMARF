ec6ebda15bfb460971fee06ecf8dfd02cb14b2c1
Piotr Jessa <dzesse@gmail.com>
2011-05-17 00:33:50 +0200

    Addin stub for MT

---
+++ core/src/main/java/org/put/hd/dmarf/algorithms/apriori/AprioriMT.java
@@ -0,0 +1,12 @@
+package org.put.hd.dmarf.algorithms.apriori;
+
+/**
+ * Multi-Threaded apriori algorithm using Java methods of parallelizations.
+ * Using Java concurrent collections.
+ * 
+ * @author Piotr
+ * 
+ */
+public class AprioriMT {
+
+}
