dea876e2d6a226311f5d6900425a70738128b385
Piotr Jessa <dzesse@gmail.com>
2011-05-26 14:40:41 +0200

    Changing the profiling tests

---
+++ core/src/test/java/org/put/hd/dmarf/profiling/TimeConsumingLoadingProfiler.java
@@ -0,0 +1,54 @@
+package org.put.hd.dmarf.profiling;
+
+import java.io.File;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.put.hd.dmarf.data.DataRepresentationBase;
+import org.put.hd.dmarf.data.builders.BasicDataBuilder;
+import org.put.hd.dmarf.data.formatters.SimpleDataFormatter;
+import org.put.hd.dmarf.data.loaders.SimpleDataLoader;
+
+/**
+ * Tests used only for profiling purposes. Snatches of code to get the loading
+ * done. Those are not JUnit test because we do not want them to be called from
+ * maven like build systems.
+ * 
+ * @author Piotr
+ * 
+ */
+public class TimeConsumingLoadingProfiler {
+
+	private SimpleDataLoader dataloader;
+	private SimpleDataFormatter formatter;
+	private BasicDataBuilder builder;
+
+	public void setUp() {
+
+		// use all the normal classes
+		builder = new BasicDataBuilder();
+		formatter = new SimpleDataFormatter(builder);
+		dataloader = new SimpleDataLoader(formatter);
+
+	}
+
+	public void time_consuming_loading() {
+		String pathToFile = "resources" + File.separator + "data"
+				+ File.separator + "pumsb_star.dat";
+		dataloader.setInputFileName(pathToFile);
+		DataRepresentationBase data = dataloader.loadData();
+
+		// get nothing with this data
+		int size = data.getTransactions().size();
+
+		System.out.println("Data loaded:" + size);
+	}
+
+	public static void main(String[] args) {
+		TimeConsumingLoadingProfiler prof = new TimeConsumingLoadingProfiler();
+
+		prof.setUp();
+		prof.time_consuming_loading();
+	}
+}
